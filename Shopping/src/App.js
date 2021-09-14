53a2ab6df4acececf30a55a2a47b5632a11bfa57
GaSeong_05 <82383294+NohGaSeong@users.noreply.github.com>
2021-09-14 15:16:38 +0900

    Add files via upload

--- Shopping/src/App.js
+++ Shopping/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import { Button,Navbar , Container,Nav,NavDropdown,Jumbotron} from 'react-bootstrap';
 import './App.css';
+import Data from './data.js';
 
 function App() {
+
+  let [shoes, shoes변경] = useState(Data);
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
   <Container>
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Brand href="#home">LUNATIC</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
         <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <Nav.Link href="#link">Introduction</Nav.Link>
+        <NavDropdown title="Category" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Shoes</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Outer</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Bottoms</NavDropdown.Item>
           <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.4">Accessories</NavDropdown.Item>
         </NavDropdown>
       </Nav>
     </Navbar.Collapse>
@@ -30,22 +33,35 @@ function App() {
 
     <div class="p-5 mb-4 bg-light rounded-3 background">
       <div class="container-fluid py-5">
-        <h1 class="display-5 fw-bold">Custom jumbotron</h1>
-        <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-        <button class="btn btn-primary btn-lg" type="button">Example button</button>
+        <h1 class="display-5 fw-bold">LUNATIC</h1>
+        <p class="col-md-8 fs-4">대충 설명</p>
+        <button class="btn btn-primary btn-lg" type="button">구매하러가기</button>
       </div>
     </div>
 </div>
 
-<div className = "container">
-  <div className = "row">
-    <div className = "col-md-4">dfdfd</div>
-    <div className = "col-md-4">dfdfd</div>
-    <div className = "col-md-4">dfdfd</div>
-  </div>
-</div>
-</div>
-  );
+
+<div className="container">
+        <div className="row">
+          { 
+            shoes.map((a,i)=>{
+             return<Card shoes={shoes[i]} i={i} />
+            })
+          }
+        </div>
+      </div>
+    </div>
+  )
 }
 
+
+function Card(props){
+  return (
+    <div className="col-md-4">
+      <img src={ 'https://codingapple1.github.io/shop/shoes' + (props.i+1) + '.jpg' } width="100%"/>
+      <h4>{ props.shoes.title }</h4>
+      <p>{ props.shoes.content } & { props.shoes.price }</p>
+    </div>
+  )
+}
 export default App;
